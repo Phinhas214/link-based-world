ccd83c7ae2899446ec30721e082d11acbfb42852
Phinhas214 <phinhasasmelash@gmail.com>
2024-04-15 19:33:08 -0700

    first commit to game implementation

--- rules.js
+++ rules.js
@@ -1,27 +1,35 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
-        this.engine.addChoice("Begin the story");
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
+        //this.engine.addChoice("Begin the story");
+        this.engine.addChoice("Start from the Top!");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        //console.log(this.engine.storyData.Locations[key]);
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
+        
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
+        //to show the location has already been discovered
+        //let discovered = 1;
+        
+        if(locationData.Choices) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
             this.engine.addChoice("The end.")
         }
+
+        
     }
 
     handleChoice(choice) {
@@ -41,4 +49,5 @@ class End extends Scene {
     }
 }
 
-Engine.load(Start, 'myStory.json');
+//Engine.load(Start, 'myStory.json');
+Engine.load(Start, 'earthStory.json');
